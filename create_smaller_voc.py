286d157f5f3b3cf195538f63b59fd349f83da2bc
finleylee2507 <46691077+finleylee2507@users.noreply.github.com>
2021-09-10 21:36:48 -0500

    Small changes.

--- create_smaller_voc.py
+++ create_smaller_voc.py
@@ -8,21 +8,21 @@ from optparse import OptionParser
 # the method randomly selects k images from the original VOC directory and copies over the corresponding annotations files
 def create_smaller_voc(k):
 
-    original_img_dir = '../NEW_TRAIN/VOCdevkit/VOC2012/JPEGImages'
-    original_annotation_dir = '../NEW_TRAIN/VOCdevkit/VOC2012/Annotations'
-    original_train_val_dir = '../NEW_TRAIN/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt'
+    original_img_dir = './NEW_TRAIN/VOCdevkit/VOC2012/JPEGImages'
+    original_annotation_dir = './NEW_TRAIN/VOCdevkit/VOC2012/Annotations'
+    original_train_val_dir = './NEW_TRAIN/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt'
 
     # create new directories and files
     # if not os.path.exists('../TRAIN_EXPERIMENT'):
     # 	os.makedirs('../TRAIN_EXPERIMENT')
-    if not os.path.exists('../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/Annotations'):
-        os.makedirs('../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/Annotations')
+    if not os.path.exists('./TRAIN_EXPERIMENT/VOCdevkit/VOC2012/Annotations'):
+        os.makedirs('./TRAIN_EXPERIMENT/VOCdevkit/VOC2012/Annotations')
     if not os.path.exists('../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/JPEGImages'):
-        os.makedirs('../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/JPEGImages')
+        os.makedirs('./TRAIN_EXPERIMENT/VOCdevkit/VOC2012/JPEGImages')
     if not os.path.exists('../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/ImageSets/Main'):
-        os.makedirs('../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/ImageSets/Main')
+        os.makedirs('./TRAIN_EXPERIMENT/VOCdevkit/VOC2012/ImageSets/Main')
 
-    f = open('../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt', "w")
+    f = open('./TRAIN_EXPERIMENT/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt', "w")
 
     counter = 0
     # iterate over the image folder
@@ -38,11 +38,11 @@ def create_smaller_voc(k):
             if os.path.exists(os.path.join(original_annotation_dir, annotation_name)) and random.random() >= 0.5 and counter < k:
                 # copy annotation file over
                 copyfile(os.path.join(original_annotation_dir, annotation_name), os.path.join(
-                    '../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/Annotations', annotation_name))
+                    './TRAIN_EXPERIMENT/VOCdevkit/VOC2012/Annotations', annotation_name))
 
                 # copy image over
                 copyfile(os.path.join(original_img_dir, filename), os.path.join(
-                    '../TRAIN_EXPERIMENT/VOCdevkit/VOC2012/JPEGImages', filename))
+                    './TRAIN_EXPERIMENT/VOCdevkit/VOC2012/JPEGImages', filename))
 
                 # write the filename to the new .txt file
                 line_to_write = train_val_dir_filename+"\n"
